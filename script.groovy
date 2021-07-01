e4b535ea80654b25e4bce00c8cd741b1a83ff93a
VuTuongBach98 <1653005@student.hcmus.edu.vn>
2021-07-01 16:10:15 +0700

    bachvu

--- script.groovy
+++ script.groovy
@@ -1,17 +1,17 @@
 def deployApp()
 {
-    build job: 'BUILD_ANDROID_APP_2';
+    build job: 'App';
 }
 def CopyAppToAutomation()
 {
-    sh "cp -r ${JENKINS_HOME}/workspace/BUILD_ANDROID_APP_2/app/build/outputs/apk/debug/app-debug.apk ${WORKSPACE}/src/test/resources/app/appBach.apk"
+    sh "cp -r ${JENKINS_HOME}/workspace/App/app/build/outputs/apk/debug/app-debug.apk ${WORKSPACE}/src/test/resources/app/appBach.apk"
 
     
 }
 
 def deployAutomationAppium()
 {
-    sh "mvn test -P abc"
+    sh "mvn test -P BachVu"
 }
 
 
